3b0baa1dabfd29158fbdc6b023a057237b261c6d
Pablo Speciale <pablo.speciale@gmail.com>
2013-11-12 15:03:47 +0100

    First version displaying points  using opencv

--- samples/tracking/main.cc
+++ samples/tracking/main.cc
@@ -2,7 +2,13 @@
 #include <vector>
 
 #include <cuimg/profiler.h>
+#include <cuimg/video_capture.h>
+#include <cuimg/improved_builtin.h>
+#include <cuimg/target.h>
+
+#include <cuimg/copy.h>
 #include <cuimg/dsl/all.h>
+// #include <cuimg/gpu/device_image2d.h>
 #include <cuimg/cpu/host_image2d.h>
 
 #include <cuimg/tracking2/tracker.h>
@@ -40,7 +46,7 @@ void update_trajectories(std::vector<trajectory>& v, TR& pset)
       assert(parts[i].age != 1 || v[i].history.empty());
       v[i].history.push_back(parts[i].pos);
       v[i].alive = true;
-      if (v[i].history.size() > 20) v[i].history.pop_front();
+      if (v[i].history.size() > 10) v[i].history.pop_front();
     }
     else
     {
@@ -48,34 +54,77 @@ void update_trajectories(std::vector<trajectory>& v, TR& pset)
     }
 }
 
+void draw_trajectory(trajectory &current, cv::Mat &img)
+{
+// 	printf("current trajectory...\n");
+	if (current.alive)
+	{
+		int n = current.history.size();
+		int i=0;
+		for(auto it=current.history.begin(); it!=current.history.end(); ++it)
+		{
+// 			std::cout << "r: " << it->r() << " - c: "<< it->c() << std::endl;
+// 			cv::Point p(it->r(), it->c());
+			cv::Point p(it->c(), it->r());
+			cv::line(img, p, p, cv::Scalar(255*i/n, 124*i/n + 125, 0), 2);
+			i++;
+		}
+	}
+
+
+// 	// show last one
+// 	if (current.alive)
+// 	{
+// 		i_int2 i_p = current.history.back();
+// 		cv::Point p(i_p.c(), i_p.r());
+// 		cv::line(img, p, p, cv::Scalar(255,0,0), 2);
+// 	}
+
+}
+
+void draw_trajectories(std::vector<trajectory> &trajectories, cv::Mat &img)
+{
+    for (unsigned i = 0; i < trajectories.size(); i++)
+	{
+		draw_trajectory(trajectories[i], img);
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
   cv::VideoCapture video;
 
   if (argc == 4)
   {
-    video.open(argv[1]);
+    video.open(argv[3]);
+  }
+  else if (argc == 3)
+  {
+    // try open camera
+    video.open(0);
   }
   else
   {
-    std::cout << "Usage: ./tracking_qt video_file nscales detector_threshold" << std::endl;
+    std::cout << "Usage: ./tracking_sample nscales detector_threshold [video_file]" << std::endl;
     return -1;
   }
 
   if(!video.isOpened())
   {
-    std::cout << "Cannot open " << argv[1] << std::endl;
+    std::cout << "Cannot open " << argv[3] << std::endl;
     return -1;
   }
 
-  int NSCALES = atoi(argv[2]);
+  int NSCALES = atoi(argv[1]);
   if (NSCALES <= 0 or NSCALES >= 10)
   {
     std::cout << "NSCALE should be > 0 and < 10, got " << argv[2] << std::endl;
     return -1;
   }
 
-  int detector_threshold = atoi(argv[3]);
+  // Detector threshold (lower it for more points) - 10 default
+  int detector_threshold = atoi(argv[2]);
 
   obox2d domain(video.get(CV_CAP_PROP_FRAME_HEIGHT), video.get(CV_CAP_PROP_FRAME_WIDTH));
   host_image2d<gl8u> frame_gl(domain);
@@ -93,9 +142,14 @@ int main(int argc, char* argv[])
   // Record trajectories at each scales.
   std::vector<std::vector<trajectory> > trajectories(NSCALES);
 
+  cv::namedWindow( "input", CV_WINDOW_AUTOSIZE );// Create a window for display.
   cv::Mat input_;
-  while (video.read(input_)) // For each frame
+//   while (video.read(input_)) // For each frame
+  for (;;)
   {
+    video >> input_;
+
+//     printf("processing...\n");
     host_image2d<i_uchar3> frame(input_);
     frame_gl = get_x(frame); // Basic Gray level conversion.
     tr1.run(frame_gl);
@@ -107,6 +161,12 @@ int main(int argc, char* argv[])
       // Update trajectories.
       update_trajectories(trajectories[s], tr1.scale(s).pset());
     }
+
+    int s = 0;
+	draw_trajectories(trajectories[s], input_);
+	cv::imshow("input", input_);
+    cv::waitKey(1);
+
   }
 
   return 0;
